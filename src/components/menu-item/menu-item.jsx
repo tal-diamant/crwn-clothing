2d45401f364ea07a567be8b713d359976df56ada
tal-diamant <erkeliwood@gmail.com>
2022-03-01 19:50:02 +0200

    added basic routing (not for all pages)

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,7 +1,8 @@
+import { withRouter } from 'react-router-dom';
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`menu-item ${size}`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div
       style={{
         backgroundImage: `url(${imageUrl})`,
@@ -15,4 +16,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
